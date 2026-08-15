05755b770e74bc7a282ed3eddfeae1550964338d
Walter Schaub <Walter@MacBook-Air-8.local>
2026-08-15 09:34:01 +0200

    Icon Kandidat an HR: neues Motiv (HR2) eingesetzt

--- wwwroot/employees.js
+++ wwwroot/employees.js
@@ -13742,7 +13742,7 @@ function _raTilesHtml() {
     // Reihenfolge Bewerbung zuerst (Walter 28.07.2026).
     const tile = (img, title, onclick) => `
         <button type="button" class="ra-tile" onclick="${onclick}">
-            <img src="img/${encodeURI(img)}?v=20260815a" alt="" loading="lazy">
+            <img src="img/${encodeURI(img)}?v=20260815b" alt="" loading="lazy">
             <span>${title}</span>
         </button>`;
     const kontoTiles = selectedEmployee?.isPayrollExcluded ? '' : `
